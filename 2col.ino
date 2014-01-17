e4d310ee0860e230fbda7de9cd320d4983ad4982
Christopher Lais <chris+rmm@zenthought.org>
2014-01-17 16:38:59 -0600

    Add build script, move libgfx
    Added Makefile using arduino-mk (Debian packaged).
    Moved libgfx to libraries for the build script.
    
    make
    make upload

---
+++ 2col.ino
@@ -0,0 +1,33 @@
+#include <gfx.h>
+
+#define COLS	2
+#define ROWS	6
+
+color_t fb_buf[COLS*ROWS];
+fb_t fb = {
+	.width = COLS,
+	.height = ROWS,
+	.pitch = COLS,
+	.buf = fb_buf,
+	.end = &fb_buf[COLS*ROWS],
+};
+
+void setup()
+{
+	ws28_init();
+}
+
+void loop()
+{
+	static int offset = 0;
+
+	fb_fill(&fb, COLOR(0,0,0));
+	font_draw(&fb, offset, 0, COLOR(16,64,64), "Hello, world");
+	font_draw(&fb, offset + 37, 0, COLOR(16,64,64), "Hello, world");
+	if (--offset < 37)
+		offset = 0;
+
+	ws28_send(&fb);
+
+	delay(1000 / 10);
+}
---
+++ Makefile
@@ -0,0 +1,14 @@
+ARDUINO_DIR := /usr/share/arduino
+ARDMK_DIR   := /usr
+AVR_TOOLS_DIR := /usr
+USER_LIB_PATH := libraries
+
+BOARD_TAG    = atmega328
+MONITOR_PORT = /dev/ttyUSB*
+ARDUINO_LIBS = SPI libgfx
+
+CFLAGS += -Wall
+EXTRA_CFLAGS += -std=c99
+
+include /usr/share/arduino/Arduino.mk
+
