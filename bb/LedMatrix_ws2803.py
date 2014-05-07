98f8e4ae72db97eafb5f502fa95417b97978e985
Christopher Lais <chris+rmm@zenthought.org>
2014-05-07 11:52:15 -0500

    bb: Update LedMatrix_ws2803 to use new SPI library

--- bb/LedMatrix_ws2803.py
+++ bb/LedMatrix_ws2803.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python
-from time import time, sleep
+from time import time
 from PIL import Image
-from Adafruit_BBIO.SPI import SPI
+import pyximport; pyximport.install()
+from RMM_SPI import SPI, SPI_MODE_0, SPI_NO_CS
 
 class LedMatrix:
-	def __init__(self, size = (29, 6), spi = SPI(0,0), clock = 25*1000*1000):
-		self.spi = spi
-		self.spi.msh = clock
-		self.spi.lsbfirst = False
-		self.spi.mode = 0b00
-		self.spi.threewire = False
+	def __init__(self, size = (29, 6), spi = None, clock = 12*1000*1000):
+		self.spi = spi if spi is not None else SPI()
+		self.spi.mode = SPI_MODE_0 | SPI_NO_CS
 		self.spi.bpw = 8
-		self.spi.writebytes([])
+		self.spi.speed_hz = clock
 		self.reftime = time()
 
 		self.size = size
 
 	def put(self, img):
-		# ensure that 600us have passed since the last draw.
-		curtime = time()
-		if (curtime - self.reftime < 600e-6):
-			sleep(600e-6 + self.reftime - curtime)
-
 		# crop to our display area and rotate to row-major
-		rimg = img.crop((0, 0, self.size[0], self.size[1])).convert('RGB').transpose(Image.ROTATE_90)
+		rimg = img.crop((0, 0, self.size[0], self.size[1])).convert('RGB').transpose(Image.ROTATE_270).transpose(Image.FLIP_LEFT_RIGHT)
+		data = bytearray(rimg.tostring())
 
-		# convert the image in to a list of ints we can send via spi.writebytes
-		data = list( ord(value) for value in rimg.tostring() )
+		# XXX FIXME: *Why* does the data need to be reversed?  Driver problem?
+		data.reverse()
 
-		self.spi.mode = 0b11 # keep clock high to avoid position reset
-		while data:
-			out = data[0:255] # switch to 16383(4?) once Adafruit_BBIO.SPI is updated.
-			del data[0:255]
-			self.spi.writebytes(out)
-		self.spi.mode = 0b00
-		self.spi.writebytes([]) # bring clock low to allow for position reset
+		# ensure that 600us have passed since the last draw.
+		# XXX TODO: maybe just add this to the last transaction always..
+		# Need to see if the driver is synchronous or asynchronous.
+		curtime = time()
+		if curtime - self.reftime < 600e-6:
+			delay_us = int(1e6 * (600e-6 + self.reftime - curtime))
+			if delay_us <= 0: delay_us = None
+		else:
+			delay_us = None
+
+		transactions = [ { 'tx': data } ]
+		if delay_us is not None:
+			transactions.insert(0, { 'tx': 0, 'delay_us': delay_us })
+		# WS2803 has strange behavior when all 144 bits (18 bytes) are not sent.
+		if len(data) % 18:
+			transactions.append({ 'tx': 18 - (len(data) % 18) })
+		self.spi.transact(transactions)
 		self.reftime = time()
